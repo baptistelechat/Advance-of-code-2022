125d8e70766ded24007b0c89bbbe979e02e0fba2
baptistelechat <baptistelechat@outlook.fr>
2022-12-05 12:02:40 +0100

    Day 5 - Supply Stacks

--- Day 5 - Supply Stacks/index.js
+++ Day 5 - Supply Stacks/index.js
@@ -1,35 +1,92 @@
 const { readFileSync } = require("fs");
 
-const port = [
-  ["R", "H", "M", "P", "Z"],
-  ["B", "J", "C", "P"],
-  ["D", "C", "L", "G", "H", "N", "S"],
-  ["L", "R", "S", "Q", "D", "M", "T", "F"],
-  ["M", "Z", "T", "B", "Q", "P", "S", "F"],
-  ["G", "B", "Z", "S", "F", "T"],
-  ["V", "R", "N"],
-  ["M", "C", "V", "D", "T", "L", "G", "P"],
-  ["L", "M", "F", "J", "N", "Q", "W"],
-];
+// Part One
+function readFile1(filename) {
+  const port = [
+    ["Z", "P", "M", "H", "R"], // 1
+    ["P", "C", "J", "B"], // 2
+    ["S", "N", "H", "G", "L", "C", "D"], // 3
+    ["F", "T", "M", "D", "Q", "S", "R", "L"], // 4
+    ["F", "S", "P", "Q", "B", "T", "Z", "M"], // 5
+    ["T", "F", "S", "Z", "B", "G"], // 6
+    ["N", "R", "V"], // 7
+    ["P", "G", "L", "T", "D", "V", "C", "M"], // 8
+    ["W", "Q", "N", "J", "F", "M", "L"], // 9
+  ];
 
-function readFile(filename) {
   const contents = readFileSync(filename, "utf-8");
 
   const arr = contents.split(/\r?\n/);
 
-  let topContainer = "";
+  // Part One
+  let res1 = [];
 
   for (let i = 0; i < arr.length; i++) {
     const instruction = [
-      arr[i].split(" ")[1], // Move
-      arr[i].split(" ")[3], // From
-      arr[i].split(" ")[5], // To
+      parseInt(arr[i].split(" ")[1]), // Move
+      parseInt(arr[i].split(" ")[3]), // From
+      parseInt(arr[i].split(" ")[5]), // To
     ];
 
-    for (let i = 0; i < instruction[0].length; i++) {
-      const element = port[instruction[1][-1]];
+    for (let i = 0; i < instruction[0]; i++) {
+      const element = port[instruction[1] - 1].pop();
+      port[instruction[2] - 1].push(element);
     }
   }
+
+  for (let i = 0; i < port.length; i++) {
+    res1.push(port[i][port[i].length - 1]);
+  }
+
+  console.log("🏆 Part 1 :", res1.toString().replaceAll(",", ""));
 }
 
-readFile("./data.txt");
+// Part Two
+function readFile2(filename) {
+  const port = [
+    ["Z", "P", "M", "H", "R"], // 1
+    ["P", "C", "J", "B"], // 2
+    ["S", "N", "H", "G", "L", "C", "D"], // 3
+    ["F", "T", "M", "D", "Q", "S", "R", "L"], // 4
+    ["F", "S", "P", "Q", "B", "T", "Z", "M"], // 5
+    ["T", "F", "S", "Z", "B", "G"], // 6
+    ["N", "R", "V"], // 7
+    ["P", "G", "L", "T", "D", "V", "C", "M"], // 8
+    ["W", "Q", "N", "J", "F", "M", "L"], // 9
+  ];
+
+  const contents = readFileSync(filename, "utf-8");
+
+  const arr = contents.split(/\r?\n/);
+
+  // Part One
+  let res2 = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    const instruction = [
+      parseInt(arr[i].split(" ")[1]), // Move
+      parseInt(arr[i].split(" ")[3]), // From
+      parseInt(arr[i].split(" ")[5]), // To
+    ];
+
+    port[instruction[2] - 1].push(
+      ...port[instruction[1] - 1].slice(
+        port[instruction[1] - 1].length - instruction[0]
+      )
+    );
+
+    port[instruction[1] - 1].splice(
+      port[instruction[1] - 1].length - instruction[0],
+      instruction[0]
+    );
+  }
+
+  for (let i = 0; i < port.length; i++) {
+    res2.push(port[i][port[i].length - 1]);
+  }
+
+  console.log("🏆 Part 2 :", res2.toString().replaceAll(",", ""));
+}
+
+readFile1("./data.txt");
+readFile2("./data.txt");
