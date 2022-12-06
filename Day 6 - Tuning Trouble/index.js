62f4ff946281db9a48e8d32eddd8c00400efa131
baptistelechat <baptistelechat@outlook.fr>
2022-12-06 09:10:08 +0100

    Day 6 - Tuning Trouble

--- Day 6 - Tuning Trouble/index.js
+++ Day 6 - Tuning Trouble/index.js
@@ -1,9 +1,17 @@
 const { readFileSync } = require("fs");
 
-function hasRepeatedLetters(str) {
-  var patt = /^([a-z])\1+$/;
-  var result = patt.test(str);
-  return result;
+function hasRepeatedCharacters(str) {
+  // Créer un ensemble vide pour stocker les caractères uniques
+  const uniqueChars = new Set();
+
+  // Parcourir chaque caractère de la chaîne
+  for (const char of str) {
+    // Ajouter le caractère à l'ensemble des caractères uniques
+    uniqueChars.add(char);
+  }
+
+  // Vérifier si le nombre d'éléments dans l'ensemble est inférieur à la longueur de la chaîne
+  return uniqueChars.size < str.length;
 }
 
 // Part One
@@ -13,12 +21,23 @@ function readFile(filename) {
   const arr = contents.split(/\r?\n/).toString();
 
   // Part One
-  let res1 = [];
-
   for (let i = 0; i < arr.length; i++) {
     if (i > 2) {
       const marker = arr.slice(i - 3, i + 1);
-      console.log(hasRepeatedLetters(marker));
+      if (hasRepeatedCharacters(marker) === false) {
+        console.log("🏆 Résultat 1 :", i+1);
+        break;
+      }
+    }
+  }
+  // Part One
+  for (let i = 0; i < arr.length; i++) {
+    if (i > 12) {
+      const marker = arr.slice(i - 13, i + 1);
+      if (hasRepeatedCharacters(marker) === false) {
+        console.log("🏆 Résultat 2 :", i+1);
+        break;
+      }
     }
   }
 }
